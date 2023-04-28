9d8dd9c7ce87cc6ec7e0cb6c72521c308394815d
Jurgita <jurgita.eicinaite@yahoo.com>
2023-04-28 18:44:42 +0300

    more than 2 version implementation

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-import { secondVersion } from "./components/scriptv2.js";
 import { firstVersion } from "./components/scriptv1.js";
+import { secondVersion } from "./components/scriptv2.js";
 
 const storageKey = 'scriptSrc';
-const versionBtn = document.querySelector('#version');
 
-localStorage.getItem(storageKey) && localStorage.getItem(storageKey) === 'v2' ? secondVersion() : firstVersion();
+const versionBtn = document.querySelector('#version');
+const currentLocal = +localStorage.getItem(storageKey);
+const funcs = [firstVersion, secondVersion];
+const version = Array(funcs.length).fill(0).map((_, i) => '' + (i + 1));
+
+versionBtn.textContent = 'Version ' + (currentLocal && currentLocal === funcs.length ? 1 : currentLocal + 1);
+
+currentLocal ? funcs[+localStorage.getItem(storageKey) - 1]() : firstVersion();
+
+function changeScriptSrc() {
+    // currentLocal !== funcs.length ? localStorage.setItem(storageKey, '' + (currentLocal + 1)) : localStorage.setItem(storageKey, '1');
+
+    // versionBtn.textContent = 'Version' + (localStorage.getItem(storageKey));
+    // funcs[+localStorage.getItem(storageKey) - 1]();
+
+    if (currentLocal !== funcs.length) {
+        localStorage.setItem(storageKey, '' + (currentLocal + 1));
+        versionBtn.textContent = 'Version ' + (+localStorage.getItem(storageKey) + 1);
+    } else {
+        localStorage.setItem(storageKey, '1');
+        versionBtn.textContent = 'Version ' + 1;
+    }
+    funcs[+localStorage.getItem(storageKey) - 1]();
+    location.reload(true);
+
+}
+
+versionBtn.addEventListener('click', changeScriptSrc);
+
+/* localStorage.getItem(storageKey) && localStorage.getItem(storageKey) === 'v2' ? secondVersion() : firstVersion();
 versionBtn.textContent = localStorage.getItem(storageKey) === 'v2' ? 'Version 1' : 'Version 2';
 
 
@@ -21,4 +49,4 @@ function changeScriptSrc() {
 
 }
 
-versionBtn.addEventListener('click', changeScriptSrc);
+versionBtn.addEventListener('click', changeScriptSrc); */
