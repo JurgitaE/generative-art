c7c53811e2ee6d86e269d641144774492995731e
Jurgita <jurgita.eicinaite@yahoo.com>
2023-04-29 09:16:25 +0300

    validation to version button

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ import { thirdVersion } from "./components/scriptv3.js";
 const versionBtn = document.querySelector('#version');
 const funcs = [firstVersion, secondVersion, thirdVersion];
 const storageKey = 'scriptSrc';
-const currentLocal = +localStorage.getItem(storageKey);
+const currentLocal = isFinite(+localStorage.getItem(storageKey)) ? +localStorage.getItem(storageKey) : localStorage.getItem(storageKey);
 
+console.log(currentLocal);
 
-versionBtn.textContent = 'Version ' + (currentLocal && currentLocal === funcs.length ? 1 : currentLocal + 1);
+versionBtn.textContent = 'Version ' + (currentLocal && currentLocal === funcs.length ? 1
+    : !currentLocal || !(typeof currentLocal === 'number') || currentLocal > funcs.length ? 2
+        : currentLocal + 1);
 currentLocal && typeof currentLocal === 'number' && currentLocal <= funcs.length ? funcs[+localStorage.getItem(storageKey) - 1]() : firstVersion();
 
 function changeScriptSrc() {
-    currentLocal && typeof currentLocal === 'number' && currentLocal < funcs.length ? localStorage.setItem(storageKey, '' + (currentLocal + 1)) : localStorage.setItem(storageKey, '1');
+    currentLocal && typeof currentLocal === 'number' && currentLocal < funcs.length ? localStorage.setItem(storageKey, '' + (currentLocal + 1))
+        : !currentLocal || !(typeof currentLocal === 'number') || currentLocal > funcs.length ? localStorage.setItem(storageKey, '2')
+            : localStorage.setItem(storageKey, '1');
+
     funcs[+localStorage.getItem(storageKey) - 1]();
     location.reload(true);
 }
